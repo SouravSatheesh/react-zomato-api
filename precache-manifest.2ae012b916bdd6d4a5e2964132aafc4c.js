73179ec02a7755a34656c33cc4822eced4fca160
SouravSatheesh <souravsatheesh1@gmail.com>
2020-08-21 16:58:34 +0530

    Updates

---
+++ precache-manifest.2ae012b916bdd6d4a5e2964132aafc4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efe48a326d35d575c39a5fb14e55b200",
+    "url": "/react-zomato-api/index.html"
+  },
+  {
+    "revision": "ed51a53b5e34ee1a8818",
+    "url": "/react-zomato-api/static/css/main.a3864f47.chunk.css"
+  },
+  {
+    "revision": "9a7a29eb5a6efbb33211",
+    "url": "/react-zomato-api/static/js/2.2c872027.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-zomato-api/static/js/2.2c872027.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed51a53b5e34ee1a8818",
+    "url": "/react-zomato-api/static/js/main.c78bf4b3.chunk.js"
+  },
+  {
+    "revision": "0ff2145114febf810ac2",
+    "url": "/react-zomato-api/static/js/runtime-main.b18794d1.js"
+  },
+  {
+    "revision": "5639812013b1fc90b099740731917bb1",
+    "url": "/react-zomato-api/static/media/Restaurant-Food.56398120.jpg"
+  }
+]);
